f1f809aacdfb882915b75f9a0cbd4666e17bcc35
onionchop <ruthhyo@gmail.com>
2022-06-08 02:12:15 -0400

    started on functions. trial and error

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,19 @@
 var currentDay = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-$("#currDay").text(currentDay);
+    $("#currDay").text(currentDay);
 
-var rows = document.getElementsByClass('row time-block past');
+var currentHr = (moment().hour());
+var description = $(".description");
+
+function hrTimes() {
+    for (i = 9; i < 18; i++){
+        var rowHr = $("hour-${i}");
+        if (i < currentHr) {
+            rowHr.addClass("past")
+        } else if (i === currentHr) {
+            rowHr.addClass("present");
+        } else (i > currentHr) {
+            rowHr.addClass("future");
+        }
+    }
+}
+
