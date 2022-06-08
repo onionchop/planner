e31f8598dfaa945cc85b2ff90802eabf66bccac7
onionchop <ruthhyo@gmail.com>
2022-06-08 02:32:22 -0400

    fixed css link

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,22 @@
-var currentDay = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-    $("#currDay").text(currentDay);
-
 var currentHr = (moment().hour());
 var description = $(".description");
 
+function currentDate(){
+    var currentDay = document.querySelector("#currentDay")
+    var time = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+    currentDay.innerHTML = time;
+}
+
+
 function hrTimes() {
     for (i = 9; i < 18; i++){
-        var rowHr = $("hour-${i}");
+        var rowHr = $("#hour-${i}");
         if (i < currentHr) {
             rowHr.addClass("past")
         } else if (i === currentHr) {
             rowHr.addClass("present");
-        } else (i > currentHr) {
+        } else if (i > currentHr) {
             rowHr.addClass("future");
         }
     }
 }
-
